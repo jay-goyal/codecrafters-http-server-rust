6c82c4dc216b76d11a6d1f8bc502acd1f4bd24eb
Jay Goyal <goyal.jay2003@gmail.com>
2024-03-16 19:06:11 +0530

    Pass second stage

--- src/server/handler.rs
+++ src/server/handler.rs
@@ -1,6 +1,22 @@
-use std::{io::Write, net::TcpStream};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpStream,
+};
 
 pub fn handle_requests(mut stream: TcpStream) {
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let start_line = &http_request[0];
+    let method = start_line.split(' ').nth(1).unwrap();
+
+    let response = match method {
+     "/" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n"
+    };
     stream.write_all(response.as_bytes()).unwrap();
 }
